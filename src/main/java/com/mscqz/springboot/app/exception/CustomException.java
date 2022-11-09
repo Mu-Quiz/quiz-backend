ffcf414575969c127ea310efe562a1b08a304a95
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2022-11-09 15:37:07 +0900

    feat: 예외 처리 코드 추가

---
+++ src/main/java/com/mscqz/springboot/app/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.mscqz.springboot.app.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
